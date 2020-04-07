9da5591f6484760ea76507f23404adcba457a08b
Ed Rivas <ed@unplug.studio>
2020-04-06 18:00:01 -0600

    feat: add goodbye function (#1)
    BREAKING CHANGE: you must now use `goodbye()`

--- semres/__init__.py
+++ semres/__init__.py
@@ -3,3 +3,7 @@ __version__ = "1.0.0"
 
 def hello():
     return "Hello world!"
+
+
+def goodbye(name="Joe"):
+    return "Goodbye {}".format(name)
--- tests/test_semres.py
+++ tests/test_semres.py
@@ -1,6 +1,14 @@
+import pytest
 
-from semres import hello
+from semres import hello, goodbye
 
 
 def test_hello():
     assert hello() == "Hello world!"
+
+
+@pytest.mark.parametrize(
+    "input,expected", [("joe", "Goodbye joe"), ("mary", "Goodbye mary")]
+)
+def test_goodbye(input, expected):
+    assert goodbye(input) == expected
