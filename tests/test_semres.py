731da3ccba7c053429de75ddd2668153dd8a4449
Ed Rivas <ed@unplug.studio>
2020-04-06 18:12:09 -0600

    fix: fix tests

--- tests/test_semres.py
+++ tests/test_semres.py
@@ -5,7 +5,7 @@ from semres import hello, goodbye
 
 @pytest.mark.parametrize(
     "input,expected",
-    [(0, "Hello world! 0"), ("mary", "Hello world! mary"), (None, "Hello world! ")],
+    [(0, "Hello world! 0"), ("mary", "Hello world! mary"), (None, "Hello world! None")],
 )
 def test_hello(input, expected):
     assert hello(input) == expected
